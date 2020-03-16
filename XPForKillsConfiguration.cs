bbbac143f762f17c7424d053f72ba83551dc8d03
PhaserArray <me@phaserarray.me>
2020-03-16 18:34:49 +0200

    DrownXP to BreathXP, tweak values and ordering

--- XPForKillsConfiguration.cs
+++ XPForKillsConfiguration.cs
@@ -4,34 +4,39 @@ namespace PhaserArray.XPForKills
 {
 	public class XPForKillsConfiguration : IRocketPluginConfiguration
 	{
+
+		public bool CheckSteamGroupTeamkill;
+		public bool DisableMessages;
+
 		public int KillXP;
 		public float HeadModifier;
 		public float TorsoModifier;
 		public float ArmModifier;
 		public float LegModifier;
 
-		public bool CheckSteamGroupTeamkill;
 		public int TeamkillXP;
 		public int SuicideXP;
 		public int DeathXP;
-        public int DrownXP;
+        public int BreathXP;
         public int ZombieXP;
         public int FireXP;
 		public void LoadDefaults()
 		{
+			CheckSteamGroupTeamkill = true;
+			DisableMessages = false;
+
 			KillXP = 50;
 			HeadModifier = 1.5f;
 			TorsoModifier = 1.0f;
 			ArmModifier = 0.75f;
 			LegModifier = 0.5f;
 
-			CheckSteamGroupTeamkill = true;
 			TeamkillXP = -500;
 			SuicideXP = -50;
-			DeathXP = -25;
-            DrownXP = -1000;
-            ZombieXP = -2000;
-            FireXP = -2000;
+			DeathXP = -50;
+			BreathXP = -50;
+            ZombieXP = -50;
+            FireXP = -50;
         }
 	}
 }
